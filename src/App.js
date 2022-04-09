337aabef390451fd2a6f18bf93802d8556e80ee4
Ekaterina <19kotik98@gmail.com>
2022-04-09 15:44:36 +0300

    without pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
+import PostService from "./Component/API/PostService";
 import PostFilter from "./Component/PostFilter";
 import PostForm from "./Component/PostForm";
 import PostList from "./Component/PostList";
 import Button from "./Component/UI/Button/Button";
+import Loader from "./Component/UI/Loader/Loader";
 import MyModal from "./Component/UI/Modal/MyModal";
+import Pagination from "./Component/UI/Pagination/Pagination";
+import { useFetching } from "./hooks/useFatching";
 import { usePosts } from "./hooks/usePost";
 
 import "./styles/App.scss";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+
+  const [fetchPosts, isPostLoading, postError] = useFetching(
+    async (limit, page) => {
+      const response = await PostService.getAll(limit, page);
+      setPosts(response.data);
+      const totalCount = response.headers["x-total-count"];
+      setTotalPages(getPageCount(totalCount, limit));
+    }
+  );
+
+  useEffect(() => {
+    fetchPosts(limit, page);
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -22,20 +43,31 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts(limit, page);
+  };
 
   return (
     <div className="App">
+      <button onClick={() => fetchPosts()}>get axios</button>
       <Button onClick={() => setModal(true)}> create new post</Button>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
 
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="list1"
-      />
+      {postError && <h1>Произошла ошибка ${postError}</h1>}
+      {isPostLoading ? (
+        <Loader />
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="list1"
+        />
+      )}
+      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
     </div>
   );
 }
